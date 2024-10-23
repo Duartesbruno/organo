65fe98fa5d9ac75d70b8f79b9a7d3c05b091812a
bruno.duarte <bruno.duarte@zaffari.com.br>
2024-10-23 13:26:45 -0300

    ajuste no link imgs

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -6,18 +6,18 @@ const Rodape = () => {
             <section>
                 <ul>
                     <li>
-                        <img src='/imagens/fb.png' alt='facebook'/>
+                        <img src='imagens/fb.png' alt='facebook'/>
                     </li>
                     <li>
-                        <img src='/imagens/tw.png' alt='twitter'/>
+                        <img src='imagens/tw.png' alt='twitter'/>
                     </li>
                     <li>
-                        <img src='/imagens/ig.png' alt='instagram'/>
+                        <img src='imagens/ig.png' alt='instagram'/>
                     </li>
                 </ul>
             </section>
             <section>
-                <img src='/imagens/logo.png' alt='logo' />
+                <img src='imagens/logo.png' alt='logo' />
             </section>
             <section>
                 <p>Desenvolvido por Alura.</p>
