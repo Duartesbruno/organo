7cbc3b7096d0ab91e71793a176f627b28f4ad386
bruno.duarte <bruno.duarte@zaffari.com.br>
2024-10-23 13:43:45 -0300

    Novo ajuste nas urls das imgs

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -4,7 +4,7 @@ export const Banner = () => {
     //parece HTML mais não é o nome é JSX
     return (
         <header className="banner">
-            <img src="/imagens/banner.png"  alt="O banner principal da página do Organo"/>
+            <img src={`${process.env.PUBLIC_URL}/imagens/banner.png`}  alt="O banner principal da página do Organo"/>
         </header>
     )
 }
--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,23 +1,24 @@
 import './Rodape.css'
 
 const Rodape = () => {
+    console.log(process.env);
     return (
         <footer className='footer'>
             <section>
                 <ul>
                     <li>
-                        <img src='imagens/fb.png' alt='facebook'/>
+                        <img src={`${process.env.PUBLIC_URL}/imagens/fb.png`} alt='facebook' />
                     </li>
                     <li>
-                        <img src='imagens/tw.png' alt='twitter'/>
+                        <img src={`${process.env.PUBLIC_URL}/imagens/tw.png`} alt='twitter' />
                     </li>
                     <li>
-                        <img src='imagens/ig.png' alt='instagram'/>
+                        <img src={`${process.env.PUBLIC_URL}/imagens/ig.png`} alt='instagram' />
                     </li>
                 </ul>
             </section>
             <section>
-                <img src='imagens/logo.png' alt='logo' />
+                <img src={`${process.env.PUBLIC_URL}/imagens/logo.png`} alt='logo' />
             </section>
             <section>
                 <p>Desenvolvido por Alura.</p>
